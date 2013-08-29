bc679264d3144d76463b828e9560c38c4fc6b4e3
Jeff Van Grimbergen <jeff.van.grimbergen@analoganalytics.com>
2013-08-29 16:23:56 -0500

    added some fields for new transaction

--- lib/cardspring_browse/app/transactions.rb
+++ lib/cardspring_browse/app/transactions.rb
@@ -12,7 +12,15 @@ module CardspringBrowse
       end
 
       post "/v1/transactions" do
-        result = api.post(request.path_info, params['transaction'])
+        post_data = {
+          "amount" => params["transaction"]["amount"],
+          "card_token" => params["transaction"]["card_token"]
+        }
+        post_data["purchase_date_time"] = params['transaction']['purchase_date_time'].upcase unless params['transaction']['purchase_date_time'].to_s.empty?
+        post_data["currency"] = params['transaction']['currency'].upcase unless params['transaction']['currency'].to_s.empty?
+        post_data["business_id"] = params['transaction']['business_id'] if params['business_or_store'] == 'business'
+        post_data["store_id"] = params['transaction']['store_id'] if params['business_or_store'] == 'store'
+        result = api.post(request.path_info, post_data)
         body = result.body
         body_hash = JSON.parse(body)
         unless body_hash.has_key?("error")
